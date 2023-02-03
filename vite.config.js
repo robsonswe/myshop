cc5b10d50168b2ba7c896ff86667b8a6e4fa3b91
ROSS1996 <41270745+ROSS1996@users.noreply.github.com>
2023-02-03 10:09:07 -0300

    Remove build

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: './ecommerce'
-  },
   base: "./"
 })
